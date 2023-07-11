606393c5be9b0b68d56aeb5264b8f137909e00e2
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-07-11 12:30:16 +0300

    remove orbit controller

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 THREE.ColorManagement.enabled = false;
 
@@ -126,8 +125,8 @@ camera.position.z = 0.5;
 scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 /**
  * Renderer
@@ -149,7 +148,7 @@ const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
 
 	// Update controls
-	controls.update();
+	// controls.update();
 	// effect();
 	// Render
 	renderer.render(scene, camera);
